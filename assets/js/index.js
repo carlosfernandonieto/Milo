9ae4bcd31856da3a59930f2dc515d6e91f3d8a7e
Carlos Nieto <carlos.nieto@COBONIETCAADX9.local>
2019-10-28 16:49:46 -0500

    Eventos, Noticias + JS + CSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -72,4 +72,45 @@ jQuery(document).ready(function($) {
         $( this ).addClass('is-checked');
         });
     });
+
+    // Slick
+
+    $('.slides-evento').slick({
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+    $('.slides-evento').slickLightbox({
+        src: 'src',
+        itemSelector: '.item img'
+    });
 });
